6ee0ae94b65f4f0c687fcf8162778c44e0c562a9
Flamingo <camilledrake95@gmail.com>
2019-04-17 21:49:05 -0500

    Updating PHP

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,85 +1,27 @@
 <?php
 require '../vendor/autoload.php';
 
-/*
-  PHP contact form script
-  Version: 1.1
-  Copyrights BootstrapMade.com
-*/
+if(isset($_POST['sendemail']))
+{
+  $name= filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+  $email_id= filter_var( $_POST['email'], FILTER_SANITIZE_EMAIL);
+  $subject= filter_var($_POST['subject'], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+  $message= filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-/***************** Configuration *****************/
-
-  // Replace with your real receiving email address
-  $contact_email_to ="camilledrake95@gmail.com";
   $email = new \SendGrid\Mail\Mail(); 
-
-  // Title prefixes
-  $subject_title =  "Contact Form Message:";
-  $name_title = "Name:";
-  $email_title = "Email:";
-  $message_title = "Message:";
-
-  // Error messages
-  $contact_error_name ="Name is too short or empty!";
-  $contact_error_email = "Please enter a valid email!";
-  $contact_error_subject = "Subject is too short or empty!";
-  $contact_error_message = "Too short message! Please enter something.";
-  
+  $email->setFrom("camilledrake95@gmail.com", "Portfolio");
+  $email->setSubject("$subject");
+  $email->addTo($email_id, $name);
+  $email->addContent("text/plain", $message);
+ 
   $sendgrid = new \SendGrid(getenv('SENDGRID_API_KEY'));
-
-/********** Do not edit from the below line ***********/
-
-  if(!isset($_SERVER['HTTP_X_REQUESTED_WITH']) AND strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest') {
-    die('Sorry Request must be Ajax POST');
-  }
-
-  if(isset($_POST)) {
-
-    $name = $email-> filter_var($_POST["name"], FILTER_SANITIZE_STRING);
-    $email = $email-> filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
-    $subject = $email-> filter_var($_POST["subject"], FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-    $message = $email-> filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-
-    if(!$contact_email_to || $contact_email_to == 'contact@example.com') {
-      die('The contact form receiving email address is not configured!');
+  if($sendgrid->send($email))
+    {
+      echo "Email sent Successfully";
+   
     }
+ 
 
-    if(strlen($name)<3){
-      die($contact_error_name);
-    }
 
-    if(!$email){
-      die($contact_error_email);
-    }
-
-    if(strlen($subject)<3){
-      die($contact_error_subject);
-    }
-
-    if(strlen($message)<3){
-      die($contact_error_message);
-    }
-
-    if(!isset($contact_email_from)) {
-      $contact_email_from = "contactform@" . @preg_replace('/^www\./','', $_SERVER['SERVER_NAME']);
-    }
-
-    $headers = 'From: ' . $name . ' <' . $contact_email_from . '>' . PHP_EOL;
-    $headers .= 'Reply-To: ' . $email . PHP_EOL;
-    $headers .= 'MIME-Version: 1.0' . PHP_EOL;
-    $headers .= 'Content-Type: text/html; charset=UTF-8' . PHP_EOL;
-    $headers .= 'X-Mailer: PHP/' . phpversion();
-
-    $message_content = '<strong>' . $name_title . '</strong> ' . $name . '<br>';
-    $message_content .= '<strong>' . $email_title . '</strong> ' . $email . '<br>';
-    $message_content .= '<strong>' . $message_title . '</strong> ' . nl2br($message);
-
-    $sendemail = $sendgrid->mail($contact_email_to, $subject_title . ' ' . $subject, $message_content, $headers);
-
-    if( $sendemail ) {
-      echo 'OK';
-    } else {
-      echo 'Could not send mail! Please check your PHP mail configuration.';
-    }
-  }
+}
 ?>
